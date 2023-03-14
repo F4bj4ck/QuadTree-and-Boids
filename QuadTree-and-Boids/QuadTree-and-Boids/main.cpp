a5a6130f412b39bc3b664230b186af7942383dd3
F4bj4ck <kraemerfa@t-online.de>
2023-03-14 20:07:32 +0100

    Initial commit after creating a window

---
+++ QuadTree-and-Boids/QuadTree-and-Boids/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(600, 400), "Quadtree! plus boids later :)");
+
+    sf::Vertex line[] = {
+        sf::Vertex(sf::Vector2f(0.f, 0.f)),
+        sf::Vertex(sf::Vector2f(150.f, 150.f))
+    };
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(line, 2, sf::Lines);
+        window.display();
+    }
+
+    return 0;
+}
